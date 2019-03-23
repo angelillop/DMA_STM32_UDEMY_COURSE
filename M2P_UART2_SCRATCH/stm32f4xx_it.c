05be77e862d149a9c99c64943bcb84801b7b6c01
angelillop <40000634+angelillop@users.noreply.github.com>
2019-03-22 17:01:53 -0700

    Update stm32f4xx_it.c
    Updating with last changes in code.

--- M2P_UART2_SCRATCH/stm32f4xx_it.c
+++ M2P_UART2_SCRATCH/stm32f4xx_it.c
@@ -15,5 +15,17 @@ void clear_exti_pending_bit(void)
 
 void EXTI15_10_IRQHandler(void)
 {
+	USART_TypeDef *pUART2;
+	pUART2 = USART2;
+	
+	//here we will send uart2_TX_DMA request to DMA1 Controller
+	pUART2->CR3 |= (1 << 7);
+	
 	clear_exti_pending_bit();
+}
+
+// IRQ handler for DMA1 stream6 global interrupt
+void DMA1_Stream6_IRQHandler(void)
+{
+	
 }
